4f3ec3f66fb2c3b180098dea35c1516c614e3f66
sinnchinn <sinathachin@gmail.com>
2024-01-09 11:24:38 -0800

    making folders

--- Controllers/AddNumbersController.cs
+++ Controllers/AddNumbersController.cs
@@ -1,20 +1,23 @@
+using ChinSMiniChallenge2_TwotoFour.services.questions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChinSMiniChallenge2_TwotoFour.Controllers;
 
 [ApiController]
 [Route("controller")]
-    public class AddNumbersController : ControllerBase
-    {
-        public int firstNumber = 0;
-        public int secondNumber = 0;
-        public int total = 0;
+public class AddNumbersController : ControllerBase
+{
+    private readonly IAddNumbersService _addNumbersService;
 
-        [HttpPost]
-        [Route("AddNumber/{firstNumber}, {secondNumber}")]
-        public int AddNumber(int firstNumber, int secondNumber)
-        {
-            total = firstNumber + secondNumber;
-            return total;
-        }
+    public AddNumbersController(IAddNumbersService addNumbersService)
+    {
+        _addNumbersService = addNumbersService;
     }
+
+    [HttpPost]
+    [Route("AddNumber/{firstNumber}, {secondNumber}")]
+    public int AddNumber(int firstNumber, int secondNumber)
+    {
+        return _addNumbersService.AddNumber(firstNumber, secondNumber);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using ChinSMiniChallenge2_TwotoFour.services;
+using ChinSMiniChallenge2_TwotoFour.services.questions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IAddNumbersService, AddNumberService>();
 
 var app = builder.Build();
 
@@ -16,7 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
